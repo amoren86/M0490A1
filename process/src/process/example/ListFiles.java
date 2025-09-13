9c8a5de3e0d85424701ce06601dbca6a7e67927c
amoren86 <amoren86@xtec.cat>
2025-09-14 00:23:59 +0200

    2025.09.14 add examples

--- process/src/process/example/ListFiles.java
+++ process/src/process/example/ListFiles.java
@@ -1,26 +1,32 @@
-package process;
+package process.example;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class ProcessExample {
+public class ListFiles {
+	// private static final String LIST_FILES = "dir"; //Windows
+	private static final String LIST_FILES = "ls"; // Linux
+
 	public static void main(String[] args) {
 		try {
-			// Start a new process (Linux command "ls")
-			Process process = Runtime.getRuntime().exec("ls");
+			// Run system command to list files
+			Process process = Runtime.getRuntime().exec(LIST_FILES);
 
-			// Read process output
+			// Get an input stream to read the standard output of the process
 			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+			System.out.println(LIST_FILES + " command output:");
 
 			String line;
 			while ((line = reader.readLine()) != null) {
-				System.out.println("Output: " + line);
+				System.out.println(line);
 			}
 
 			// Wait for the process to finish
 			int exitCode = process.waitFor();
 			System.out.println("Process finished with exit code: " + exitCode);
+
 		} catch (IOException | InterruptedException e) {
 			e.printStackTrace();
 			Thread.currentThread().interrupt();
---
+++ process/src/runtime/example/LaunchProcess.java
@@ -0,0 +1,23 @@
+package runtime.example;
+
+import java.io.IOException;
+
+public class LaunchProcess {
+	//private static final String PROGRAM = "notepad"; // Windows
+	//private static final String PROGRAM = "gedit"; // Linux
+	private static final String PROGRAM = "pluma"; // Linux
+	
+	public static void main(String[] args) {
+		try {
+			// Launch an external process
+			Process process = Runtime.getRuntime().exec(PROGRAM);
+
+			// Wait until the external process finishes
+			process.waitFor();
+
+			System.out.println(PROGRAM +" has been closed.");
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
